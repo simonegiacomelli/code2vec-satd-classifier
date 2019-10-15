9aa3e3cc484eede1ed00137379d7eceb143f0104
Simone Giacomelli <simone.giacomelli@gmail.com>
2019-10-15 08:59:11 +0200

    add parallel computing example

--- src/jvmMain/kotlin/sample/ParallelFixed.kt
+++ src/jvmMain/kotlin/sample/ParallelFixed.kt
@@ -30,9 +30,39 @@ class Main {
         Thread.sleep(1000) // simulating kafka consumer.poll(... )
         1
     }
+
+    suspend fun <A, B> Iterable<A>.pmap(f: suspend (A) -> B): List<B> = coroutineScope {
+        map { async { f(it) } }.awaitAll()
+    }
+
+    suspend fun <A, B> Iterable<A>.pmap2(f: suspend (A) -> B): List<B> = withContext(es) {
+        map { async { f(it) } }.awaitAll()
+    }
+
+    suspend fun process2() {
+        (0..9).pmap2 {
+            sharade(it)
+        }
+    }
+
+    private fun sharade(it: Int) {
+        println("${it} INIT")
+        Thread.sleep(300)
+        println("${it} CONT")
+        Thread.sleep(300)
+        println("${it} DONE")
+    }
+
+    suspend fun process3() {
+        (0..9).forEach {
+            withContext(es) {
+                sharade(it)
+            }
+        }
+    }
 }
 
 
-fun main() {
-    Main().process()
+suspend fun main() {
+    Main().process2()
 }
