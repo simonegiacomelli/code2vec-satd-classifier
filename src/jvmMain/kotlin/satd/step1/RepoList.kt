37c5fd0884014a6d67f7f412628df560987f062f
Simone Giacomelli <simone.giacomelli@gmail.com>
2019-10-19 19:28:23 +0200

    refactoring

--- src/jvmMain/kotlin/satd/step1/RepoList.kt
+++ src/jvmMain/kotlin/satd/step1/RepoList.kt
@@ -2,16 +2,19 @@ package satd.step1
 
 import java.net.URL
 
-fun repoUrlList() = RepoList.repoTxtResource()
-    .readText()
-    .split('\n')
-    .map { it.trim() }
-    .filter { !it.startsWith("#") }
-    .map { URL(it) }
+fun repoUrlList() = RepoList.repoUrlList()
 
 private class RepoList {
     companion object {
         fun repoTxtResource() = this::class.java.classLoader.getResource("satd/step1/repo-urls.txt")!!
+
+        fun repoUrlList() = repoTxtResource()
+            .readText()
+            .split('\n')
+            .map { it.trim() }
+            .filter { !it.startsWith("#") }
+            .map { URL(it) }
+
         @JvmStatic
         fun main(args: Array<String>) {
             repoUrlList().forEach {
