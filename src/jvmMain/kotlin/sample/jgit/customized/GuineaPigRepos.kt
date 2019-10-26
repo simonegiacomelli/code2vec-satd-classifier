383a2ee2b4e74de3635c54827ee1a39bdfe3ccf0
Simone Giacomelli <simone.giacomelli@gmail.com>
2019-10-26 10:49:39 +0200

    reordered class

--- src/jvmMain/kotlin/sample/jgit/customized/GuineaPigRepos.kt
+++ src/jvmMain/kotlin/sample/jgit/customized/GuineaPigRepos.kt
@@ -4,65 +4,6 @@ import satd.step1.Folders
 import org.eclipse.jgit.api.Git
 import org.eclipse.jgit.api.ResetCommand
 
-
-abstract class guinea_pig(name: String) : AutoCloseable {
-    val workTree = Folders.guineaPigRepos.resolve(name).toFile()
-
-    fun newGit() = Git.init().setDirectory(workTree).call()!!
-
-    private val lazyGit = lazy { newGit() }
-    val git by lazyGit
-
-    override fun close() {
-        if (lazyGit.isInitialized()) git.close()
-    }
-
-    fun rebuild() {
-        workTree.deleteRecursively()
-        assert(!workTree.exists())
-        workTree.mkdirs()
-        this.use {
-            build()
-        }
-        sysGit("git log --all --decorate --oneline --graph")
-    }
-
-    fun commitFile(file: String, content: String, message: String) {
-        addFile(file, content)
-        git.commit().setMessage(message).call()
-    }
-
-    fun commitFile(file: String, content: String) {
-        addFile(file, content)
-        git.commit().setMessage("Adding $file").call()
-    }
-
-    fun commitFile(file: String) {
-        addFile(file, "Content of $file")
-        git.commit().setMessage("Adding $file").call()
-    }
-
-    fun addFile(file: String, content: String) {
-        git.repository.workTree.resolve(file).writeText(content)
-        git.add().addFilepattern(file).call()
-    }
-
-    protected abstract fun build()
-
-    /**
-     * jgit does not support all git features, so we fallback to git command line
-     * e.g. octopus merge
-     */
-    fun sysGit(command: String) {
-        ProcessBuilder(command.split(' '))
-            .directory(git.repository.workTree)
-            .inheritIO()
-            .start()
-            .waitFor()
-    }
-
-}
-
 class gp1 : guinea_pig("gp1") {
 
     companion object {
@@ -175,6 +116,64 @@ class gp_three_source_dag : guinea_pig("gp_three_source_dag") {
         git.reset().setMode(ResetCommand.ResetType.HARD).call()
 
         commitFile("after_merge.txt")
+    }
+
+}
+
+abstract class guinea_pig(name: String) : AutoCloseable {
+    val workTree = Folders.guineaPigRepos.resolve(name).toFile()
+
+    fun newGit() = Git.init().setDirectory(workTree).call()!!
+
+    private val lazyGit = lazy { newGit() }
+    val git by lazyGit
+
+    override fun close() {
+        if (lazyGit.isInitialized()) git.close()
+    }
+
+    fun rebuild() {
+        workTree.deleteRecursively()
+        assert(!workTree.exists())
+        workTree.mkdirs()
+        this.use {
+            build()
+        }
+        sysGit("git log --all --decorate --oneline --graph")
+    }
+
+    fun commitFile(file: String, content: String, message: String) {
+        addFile(file, content)
+        git.commit().setMessage(message).call()
+    }
+
+    fun commitFile(file: String, content: String) {
+        addFile(file, content)
+        git.commit().setMessage("Adding $file").call()
+    }
+
+    fun commitFile(file: String) {
+        addFile(file, "Content of $file")
+        git.commit().setMessage("Adding $file").call()
+    }
+
+    fun addFile(file: String, content: String) {
+        git.repository.workTree.resolve(file).writeText(content)
+        git.add().addFilepattern(file).call()
+    }
+
+    protected abstract fun build()
+
+    /**
+     * jgit does not support all git features, so we fallback to git command line
+     * e.g. octopus merge
+     */
+    fun sysGit(command: String) {
+        ProcessBuilder(command.split(' '))
+            .directory(git.repository.workTree)
+            .inheritIO()
+            .start()
+            .waitFor()
     }
 
 }
