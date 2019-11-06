e65ca2e5c4476f140a2f5f2782384be7deb92f83
Simone Giacomelli <simone.giacomelli@gmail.com>
2019-11-06 10:03:58 +0100

    organize import

--- src/jvmMain/kotlin/satd/step2/Main.kt
+++ src/jvmMain/kotlin/satd/step2/Main.kt
@@ -4,7 +4,6 @@ import satd.step1.Folders
 import satd.step1.Repo
 import satd.step1.logln
 import satd.step1.repoUrlList
-import java.util.stream.Collectors
 import kotlin.streams.toList
 
 fun main() {
