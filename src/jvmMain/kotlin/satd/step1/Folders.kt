272e701488d395a6fc5d31b0d9c6504d37a89af7
Simone Giacomelli <simone.giacomelli@gmail.com>
2019-11-19 09:13:42 +0100

    add folder for heapdumps

--- src/jvmMain/kotlin/satd/step1/Folders.kt
+++ src/jvmMain/kotlin/satd/step1/Folders.kt
@@ -10,6 +10,7 @@ class Folders {
         val log get() = data.resolve("log")
         val cache get() = data.resolve("cache")
         val database get() = data.resolve("database")
+        val heapdumps get() = data.resolve("heapdumps")
         val database_db1 get() = database.resolve("db1").toAbsolutePath().normalize()!!
         val guineaPigRepos get() = data.resolve("guinea_pig_repos")
     }
