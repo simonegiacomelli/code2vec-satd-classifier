c6516ae8df9024233fafe39f55f88c2191b72173
Simone Giacomelli <simone.giacomelli@gmail.com>
2019-11-19 09:19:37 +0100

    refactoring

--- src/jvmMain/kotlin/satd/step2/Main.kt
+++ src/jvmMain/kotlin/satd/step2/Main.kt
@@ -3,32 +3,27 @@ package satd.step2
 import satd.utils.Repo
 import satd.utils.RepoList
 import satd.step1.*
+import satd.utils.HeapDumper
 import satd.utils.logln
 import kotlin.streams.toList
 
 fun main() {
-    Main().go()
-}
+    logln("Starting")
+    HeapDumper.enable()
 
-class Main {
-    fun go() {
-        logln("Starting")
+    if (!Folders.database_db1.toFile().deleteRecursively())
+        throw IllegalStateException("Errore removing the database ${Folders.database_db1}")
 
-        if (!Folders.database_db1.toFile().deleteRecursively())
-            throw IllegalStateException("Errore removing the database ${Folders.database_db1}")
-
-        RepoList
-//            .tenRepos
-            .androidReposFull
+    RepoList
+        .androidReposFull
 //            .take(2000)
-            .stream()
-            .parallel()
-            .map { Repo(it).clone() }
-            .filter { !it.failed }
-            .map { Find(it.newGit()).trackSatd() }
-            .toList()
+        .stream()
+        .parallel()
+        .map { Repo(it).clone() }
+        .filter { !it.failed }
+        .map { Find(it.newGit()).trackSatd() }
+        .toList()
 
-        logln("Done")
-    }
+    logln("Done")
 
 }
