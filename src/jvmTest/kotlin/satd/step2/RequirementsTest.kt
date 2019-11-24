4ad3c0fd82c5a74b2c0e5c48b44c23731f52949a
Simone Giacomelli <simone.giacomelli@gmail.com>
2019-11-24 21:14:15 +0100

    new test case

--- src/jvmTest/kotlin/satd/step2/RequirementsTest.kt
+++ src/jvmTest/kotlin/satd/step2/RequirementsTest.kt
@@ -5,13 +5,15 @@ import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
-internal class RequirementsTest {
+class RequirementsTest {
     val class1 by lazy { load("Class1.java") }
     val class2 by lazy { load("Class2.java") }
     val a1old by lazy { load("Android1_old.java") }
     val a1new by lazy { load("Android1_new.java") }
     val case3old by lazy { load("Case3_old.java") }
     val case3new by lazy { load("Case3_new.java") }
+    val stringCaseOld by lazy { load("StringCaseOld.java") }
+    val stringCaseNew by lazy { load("StringCaseNew.java") }
 
     private fun load(s: String) = this::class.java.classLoader.getResource("satd/step2/ComparerTest/$s")!!.readText()
 
@@ -27,10 +29,16 @@ internal class RequirementsTest {
         assertFalse(target.accept());
     }
 
- @Test
+    @Test
     fun `correct satd that disappear with code change should be accepted`() {
         val target = instantiateTarget(case3old, case3new, "method1")
         assertTrue(target.accept());
+    }
+
+    @Test
+    fun `a method that changes only in strings should be rejected`() {
+        val target = instantiateTarget(stringCaseOld, stringCaseNew, "method1")
+        assertFalse(target.accept());
     }
 
     private fun instantiateTarget(cold: String, cnew: String, name: String): Requirements {
