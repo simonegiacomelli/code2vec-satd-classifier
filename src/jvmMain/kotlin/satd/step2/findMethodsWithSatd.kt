c777923e3c9ec41624dd0bff3da80eb3aa6c860f
Simone Giacomelli <simone.giacomelli@gmail.com>
2019-11-28 22:03:06 +0100

    removed 2 patterns because too many FP

--- src/jvmMain/kotlin/satd/step2/findMethodsWithSatd.kt
+++ src/jvmMain/kotlin/satd/step2/findMethodsWithSatd.kt
@@ -97,9 +97,10 @@ class MethodWithSatd(
         }
 
         val patterns by lazy {
-            val content = this::class.java.classLoader.getResource("satd/step1/hack-patterns.txt")!!.readText()
+            val content = this::class.java.classLoader.getResource("satd/step2/hack-patterns.txt")!!.readText()
             content.split('\n')
                 .map { it.trim() }
+                .filter { !it.startsWith("#") }
         }
         val reg by lazy { patterns.map { Pair(it, Pattern.compile("""\b$it\b""", Pattern.CASE_INSENSITIVE)!!) } }
     }
