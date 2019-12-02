21235ee1fdbc0a7e50fd222f9d3d18b2792b4684
Simone Giacomelli <simone.giacomelli@gmail.com>
2019-12-02 10:20:46 +0100

    moved config to its file

---
+++ src/jvmMain/kotlin/satd/utils/Config.kt
@@ -0,0 +1,30 @@
+package satd.utils
+
+import java.io.File
+import java.util.*
+import kotlin.reflect.KProperty
+
+object config {
+    private val prop = Properties()
+
+
+    fun loadArgs(args: Array<String>) {
+        File(if (args.isEmpty()) "config.properties" else args[0])
+            .apply {
+                if (exists())
+                    inputStream()
+                        .use {
+                            logln("Loading: $absolutePath")
+                            prop.load(it)
+                        }
+                else
+                    logln("Config file does not exists: $absolutePath does")
+            }
+    }
+
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): String {
+        return prop.getProperty(property.name).orEmpty()
+    }
+
+    val repos_path by this
+}
--- src/jvmMain/kotlin/satd/utils/Folders.kt
+++ src/jvmMain/kotlin/satd/utils/Folders.kt
@@ -1,34 +1,6 @@
 package satd.utils
 
-import java.io.File
 import java.nio.file.Paths
-import java.util.*
-import kotlin.reflect.KProperty
-
-object config {
-    private val prop = Properties()
-
-
-    fun loadArgs(args: Array<String>) {
-        File(if (args.isEmpty()) "config.properties" else args[0])
-            .apply {
-                if (exists())
-                    inputStream()
-                        .use {
-                            logln("Loading: $absolutePath")
-                            prop.load(it)
-                        }
-                else
-                    logln("Config file does not exists: $absolutePath does")
-            }
-    }
-
-    operator fun getValue(thisRef: Any?, property: KProperty<*>): String {
-        return prop.getProperty(property.name).orEmpty()
-    }
-
-    val repos_path by this
-}
 
 
 class FolderConf(val config: config) {
