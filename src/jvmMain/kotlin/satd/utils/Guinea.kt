33d18eb590b72562c9a18fe963a535f0f1ba7b4b
Simone Giacomelli <simone.giacomelli@gmail.com>
2019-12-07 19:00:31 +0100

    20000 urls scanned for satd

--- src/jvmMain/kotlin/satd/utils/Guinea.kt
+++ src/jvmMain/kotlin/satd/utils/Guinea.kt
@@ -1,13 +1,21 @@
 package satd.utils
 
+import satd.utils.RepoList.Companion.androidReposFull
+import satd.utils.RepoList.Companion.androidReposFull2
 import java.io.PrintWriter
 import java.io.StringWriter
 import java.util.concurrent.ForkJoinPool
 
 
 fun main() {
-    commonPoolInfo()
-    exceptionToString()
+    val s1 = androidReposFull.toSet()
+    val s2 = androidReposFull2.toSet()
+
+    val s1_intersection_s2 = s1.intersect(s2)
+    println("${s1.size} ${s2.size} ${s1_intersection_s2.size}")
+    s1.subtract(s2).forEach {
+        println(it)
+    }
 }
 
 private fun exceptionToString() {
