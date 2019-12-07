8b2087219bc4ca9fc33b8bc786afdde9156c4781
Simone Giacomelli <simone.giacomelli@gmail.com>
2019-12-07 12:26:40 +0100

    first github full succesfull scan for android url repos

--- src/jvmMain/kotlin/satd/utils/GithubSearch.kt
+++ src/jvmMain/kotlin/satd/utils/GithubSearch.kt
@@ -2,6 +2,7 @@ package satd.utils
 
 import com.google.gson.JsonParser
 import org.joda.time.DateTime
+import java.io.File
 import java.io.PrintWriter
 import java.io.StringWriter
 import java.net.URL
@@ -27,7 +28,7 @@ private fun github2016() {
     for (i in 0..5) {
         val s = DateTime(2016, 1 + (i * 2), 1, 1, 1)
         val e = s.plusMonths(2).minusDays(1)
-        qryGithubDateRange(s, e)
+        qryGithubFullscanDateRange(s, e)
     }
 }
 
@@ -36,31 +37,48 @@ private fun githubYearBy15Days(year: Int) {
     while (s.year().get() == year) {
         val isFirstOfTheMonth = s.dayOfMonth().get() == 1
         val e = if (isFirstOfTheMonth) s.plusDays(14)!! else s.dayOfMonth().withMaximumValue()!!
-        qryGithubDateRange(s, e)
+        qryGithubFullscanDateRange(s, e)
         s = e.plusDays(1)
     }
 }
 
-private fun qryGithubDateRange(s: DateTime, e: DateTime) {
+class DateRange(val s: DateTime, val e: DateTime) {
     val dts = s.toString("yyyy-MM-dd").orEmpty()
     val dte = e.toString("yyyy-MM-dd").orEmpty()
-    val url = "https://api.github.com/search/repositories?" +
-            "q=language:Java+topic:android+is:public+created:$dts..$dte&page=1&per_page=100"
+    val qry = "$dts..$dte"
+    val fs = "$dts-$dte"
+}
+
+private fun qryGithubFullscanDateRange(s: DateTime, e: DateTime) {
     val g = Folders.database.resolve("github").toFile()
     g.mkdirs()
-    val jsonFile = g.resolve("$dts-$dte.json")
 
+    val dtr = DateRange(s, e)
+    val jsonFile = g.resolve("${dtr.fs}.json")
+
+    var page = 1
+    qryGitbubDtr(dtr, jsonFile, page)
+
+    val json = jsonFile.readLines().drop(1).joinToString("\n")
+    val jsonObject = JsonParser.parseString(json).asJsonObject
+
+    var totalCount = jsonObject.get("total_count").asInt - 100
+    while (totalCount > 0) {
+        page++
+        qryGitbubDtr(dtr, g.resolve("${dtr.fs}--p$page.json"), page)
+        totalCount -= 100
+    }
+}
+
+private fun qryGitbubDtr(dtr: DateRange, jsonFile: File, page: Int) {
+    val url = "https://api.github.com/search/repositories?" +
+            "q=language:Java+topic:android+is:public+created:${dtr.qry}&page=$page&per_page=100"
     println(url + (if (jsonFile.exists()) " skipping, file already exists" else ""))
     if (!jsonFile.exists()) {
         val content = URL(url).readText()
         jsonFile.writeText("$url\n$content")
-        Thread.sleep(10000)
+        Thread.sleep(20000)
     }
-
-    val json = jsonFile.readLines().drop(1).joinToString("\n")
-    val jsonObject = JsonParser.parseString(json).asJsonObject
-//    println(jsonObject)
-    println("${jsonFile.name} ${jsonObject.get("total_count")}")
 }
 
 private fun githubApiCallOneYear() {
