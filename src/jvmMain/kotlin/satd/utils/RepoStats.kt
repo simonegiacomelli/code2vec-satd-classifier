077bfd946b40d9478720924b41cbdf5260ff179c
Simone Giacomelli <simone.giacomelli@gmail.com>
2019-12-07 19:19:48 +0100

    removed verbose log

--- src/jvmMain/kotlin/satd/utils/RepoStats.kt
+++ src/jvmMain/kotlin/satd/utils/RepoStats.kt
@@ -2,14 +2,13 @@ package satd.utils
 
 import org.eclipse.jgit.api.Git
 
-data class RepoStats(val commitCount: Int, val sizeMB: Long, val path: String)
+data class RepoStats(val commitCount: Int, val sizeMB: Long)
 
 fun Git.stats(): RepoStats {
     val commitCount = this.log().all().call().count()
     val sizeBytes = pathSize(this.repository.workTree.toPath())
 
     return RepoStats(
-        path = this.repository.workTree.toPath().normalize().toAbsolutePath().toString(),
         commitCount = commitCount,
         sizeMB = sizeBytes / (1024 * 1024)
     )
