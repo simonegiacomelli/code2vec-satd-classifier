9ec1d1f0b41e9dc7b7a0179c83a205dad7834835
Simone Giacomelli <simone.giacomelli@gmail.com>
2019-12-07 09:05:24 +0100

    github query searching for android repos

--- src/jvmMain/kotlin/satd/utils/GithubSearch.kt
+++ src/jvmMain/kotlin/satd/utils/GithubSearch.kt
@@ -4,23 +4,63 @@ import org.joda.time.DateTime
 import java.io.PrintWriter
 import java.io.StringWriter
 import java.net.URL
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 import java.util.concurrent.ForkJoinPool
 
-val baseUrl =
-    "https://api.github.com/search/repositories?q=language:Java+topic:android+is:public+created:2019-01-01..2019-01-31&page=1&per_page=100"
-
 fun main() {
-    fun dateTimeToStr(): String = LocalDateTime.now().format(
-        DateTimeFormatter.ofPattern("yyyy-MM-dd--HH-mm-ss")
-    )
+    //2010-2013
+    val u01 = "2010-01-01..2013-12-31"
+    //2014
+    val u02 = "2014-01-01..2014-12-31"
+    //2015
+    val u2015a = "2015-01-01..2015-06-30"
+    val u2015b = "2015-07-01..2015-12-31"
+    //2016
+    github2016()
+    githubYearBy15Days(2017)
+    githubYearBy15Days(2018)
+    githubYearBy15Days(2019)
+}
 
+private fun github2016() {
+    for (i in 0..5) {
+        val s = DateTime(2016, 1 + (i * 2), 1, 1, 1)
+        val e = s.plusMonths(2).minusDays(1)
+        qryGithubDateRange(s, e)
+    }
+}
+
+private fun githubYearBy15Days(year: Int) {
+    var s = DateTime(year, 1, 1, 1, 1)
+    while (s.year().get() == year) {
+        val isFirstOfTheMonth = s.dayOfMonth().get() == 1
+        val e = if (isFirstOfTheMonth) s.plusDays(14)!! else s.dayOfMonth().withMaximumValue()!!
+        qryGithubDateRange(s, e)
+        s = e.plusDays(1)
+    }
+}
+
+private fun qryGithubDateRange(s: DateTime, e: DateTime) {
+    val dts = s.toString("yyyy-MM-dd").orEmpty()
+    val dte = e.toString("yyyy-MM-dd").orEmpty()
+    val url = "https://api.github.com/search/repositories?" +
+            "q=language:Java+topic:android+is:public+created:$dts..$dte&page=1&per_page=100"
     val g = Folders.database.resolve("github").toFile()
     g.mkdirs()
-    var year = 2019;
+    val jsonFile = g.resolve("$dts-$dte.json")
+
+    println(url + (if (jsonFile.exists()) " skipping, file already exists" else ""))
+    if (jsonFile.exists()) return
+    val content = URL(url).readText()
+    jsonFile.writeText("$url\n$content")
+    Thread.sleep(10000)
+}
+
+private fun githubApiCallOneYear() {
+    val g = Folders.database.resolve("github").toFile()
+    g.mkdirs()
+    var year = 2010;
     while (true) {
-        if (year < 2008)
+        if (year < 2005)
             break
         val start = DateTime(year, 1, 1, 0, 0)
         val end = DateTime(year, 12, 31, 0, 0)
@@ -37,8 +77,6 @@ fun main() {
         year--
 
     }
-
-
 }
 
 private fun exceptionToString() {
