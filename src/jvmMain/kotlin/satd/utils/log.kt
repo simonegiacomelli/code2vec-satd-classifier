3c845b0774aeae97bf9a8ad341c3028955b27fe5
Simone Giacomelli <simone.giacomelli@gmail.com>
2019-12-07 11:13:52 +0100

    write log to file

--- src/jvmMain/kotlin/satd/utils/log.kt
+++ src/jvmMain/kotlin/satd/utils/log.kt
@@ -1,7 +1,17 @@
 package satd.utils
 
+import java.io.File
+
+private val logfile by lazy {
+    val f = Folders.log.toFile().resolve("run")
+    f.mkdirs()
+    f.resolve(dateTimeToStr()+".txt").printWriter()
+}
+
+@Synchronized
 fun logln(line: String) {
     val name = Thread.currentThread().name
     val message = "${name.padEnd(50)} $line"
     println(message)
+    logfile.println(message)
 }
