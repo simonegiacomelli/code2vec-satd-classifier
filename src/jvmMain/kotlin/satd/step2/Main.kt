45c59c5ddd9f847ff70644ba94ee4c88ef5f578a
Simone Giacomelli <simone.giacomelli@gmail.com>
2019-12-13 07:40:17 +0100

    full repo list

--- src/jvmMain/kotlin/satd/step2/Main.kt
+++ src/jvmMain/kotlin/satd/step2/Main.kt
@@ -18,8 +18,7 @@ fun main(args: Array<String>) {
     logln("Using pool: $pool")
     pool.submit {
         RepoList
-//            .getUrls()
-            .testRepos
+            .getUrls()
             .also { repoRate.totRepo = it.size }
             .subtract(DbRepos.allDone().also { repoRate.alreadyDone(it.size) })
             .take(config.batch_size.toIntOrNull() ?: 1000)
