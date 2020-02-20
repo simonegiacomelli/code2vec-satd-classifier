adc53a841dafa78291ee254e79eaab095b9b2874
Simone Giacomelli <simone.giacomelli@gmail.com>
2020-02-20 12:18:16 +0100

    GithubQueryTool developing impl

--- src/jvmMain/kotlin/satd/github/GithubQueryTool.kt
+++ src/jvmMain/kotlin/satd/github/GithubQueryTool.kt
@@ -1,7 +1,12 @@
 package satd.github
 
+import com.google.gson.JsonParser
 import org.joda.time.DateTime
+import org.joda.time.Interval
+import org.joda.time.Period
 import java.io.File
+import java.net.URL
+import kotlin.math.max
 
 fun main() {
 
@@ -19,17 +24,20 @@ fun main() {
 
 }
 
-class GithubQueryTool(workingFolder: File, val dateRange: DateRange, val querySpecification: String) {
+class GithubQueryTool(val workingFolder: File, val dateRange: DateRange, val querySpecification: String) {
 
     private val jsonFolder = workingFolder.resolve("json")
     private val L = mutableListOf<ReposSearch>()
+    private val output = File(workingFolder, "github-url-list.txt")
     fun createTxt(): File {
         jsonFolder.mkdirs()
+        output.writeText("")
+
         L.add(ReposSearch(dateRange, querySpecification))
         while (L.isNotEmpty()) {
             val q = L.removeAt(0)
             val r = q.execute()
-            if (r.hits > 0)
+            if (r.total_count > 0)
                 L.addAll(0, q.split())
             else {
                 r.save()
@@ -37,25 +45,53 @@ class GithubQueryTool(workingFolder: File, val dateRange: DateRange, val querySp
                     .forEach { it.execute().save() }
             }
         }
-        return File("")
+        return output
     }
 
-    class ReposSearch(dateRange: DateRange, querySpecification: String) {
+    inner class ReposSearch(val dateRange: DateRange, val querySpecification: String, val page: Int = 1) {
+        private val jsonFile = jsonFolder.resolve("${dateRange.fs}-p$page.json")
+
         fun execute(): SearchResult {
-            TODO("not implemented")
+            val url = "https://api.github.com/search/repositories?" +
+                    "q=$querySpecification created:${dateRange.qry}&page=$page&per_page=100"
+                        .replace(" ", "+")
+
+            println(url + (if (jsonFile.exists()) " skipping, file already exists" else ""))
+
+            if (!jsonFile.exists())
+                jsonFile.writeText("$url\n${queryGithub(url)}")
+
+            val content = jsonFile.readLines().drop(1).joinToString("\n")
+
+
+
+            return SearchResult(content)
+
+        }
+
+        private fun queryGithub(url: String): String {
+            val content = URL(url).readText()
+            //this could read the headers and wait the minimum amount of time
+            //see https://developer.github.com/v3/#rate-limiting
+            Thread.sleep(20000)
+            return content
         }
 
         fun split(): List<ReposSearch> {
-            TODO("not implemented")
+            val (left, right) = dateRange.split()
+            TODO()
         }
 
-        fun followingPages():Sequence<ReposSearch> {
+        fun followingPages(): Sequence<ReposSearch> {
             TODO("not implemented")
         }
 
     }
 
-    class SearchResult(val hits: Int){
+    class SearchResult(jsonContent: String) {
+
+        private val json by lazy { JsonParser.parseString(jsonContent).asJsonObject!! }
+        val total_count: Int by lazy { json.get("total_count").asInt }
 
         fun save() {
             TODO("not implemented")
@@ -66,6 +102,18 @@ class GithubQueryTool(workingFolder: File, val dateRange: DateRange, val querySp
 
 
 class DateRange(val dtStart: DateTime, val dtEnd: DateTime) {
+    fun split(): Pair<DateRange, DateRange> {
+        if (dtStart == dtEnd) throw Exception("Cannot split one day :(")
+        val p = Period(dtStart, dtEnd).plusDays(1)
+        val days1 = p.days / 2
+        val days2 = p.days - days1
+        val days = max(days1, days2) - 1
+
+        val left = DateRange(dtStart, dtStart.plusDays(days))
+        val right = DateRange(dtStart.plusDays(days + 1), dtEnd)
+        return Pair(left, right)
+    }
+
     val start = dtStart.yyyyMMdd()
     val end = dtEnd.yyyyMMdd()
     val qry = "$start..$end"
---
+++ src/jvmTest/kotlin/satd/github/DateRangeTest.kt
@@ -0,0 +1,30 @@
+package satd.github
+
+
+import org.joda.time.DateTime
+import kotlin.test.*
+
+class DateRangeTest {
+
+    @Test
+    fun testExternalBorderDatesAreCorrect() {
+        val dtStart = DateTime.parse("2020-01-01")
+        val dtEnd = DateTime.parse("2020-01-06")
+
+        val target = DateRange(dtStart, dtEnd)
+        val (left, right) = target.split()
+        assertEquals(dtStart, left.dtStart)
+        assertEquals(dtEnd, right.dtEnd)
+    }
+
+    @Test
+    fun testInternalBordersAreCorrect() {
+        val dtStart = DateTime.parse("2020-01-01")
+        val dtEnd = DateTime.parse("2020-01-06")
+
+        val target = DateRange(dtStart, dtEnd)
+        val (left, right) = target.split()
+        assertEquals(DateTime.parse("2020-01-03"), left.dtEnd)
+        assertEquals(DateTime.parse("2020-01-04"), right.dtStart)
+    }
+}
