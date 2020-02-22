26ebb35cec53d4dfef4e7d4d9247506228cfc203
Simone Giacomelli <simone.giacomelli@gmail.com>
2020-02-22 09:21:11 +0100

    refactoring

--- src/jvmMain/kotlin/satd/github/GithubQueryTool.kt
+++ src/jvmMain/kotlin/satd/github/GithubQueryTool.kt
@@ -14,7 +14,7 @@ fun main() {
     val dtStart = DateTime(2000, 1, 1, 0, 0)
     val dtEnd = DateTime(2021, 1, 1, 0, 0)
     GithubQueryTool(
-        File("./data/github/query1")
+        File("./data/github/query2")
         , DateRange(dtStart, dtEnd)
         , "language:Java topic:android is:public"
     )
@@ -25,12 +25,13 @@ fun main() {
 
 }
 
-class GithubQueryTool(val workingFolder: File, val dateRange: DateRange, val querySpecification: String) {
+class GithubQueryTool(workingFolder: File, val dateRange: DateRange, val querySpecification: String) {
 
     private val cacheFolder = workingFolder.resolve("cache")
     private val jsonFolder = workingFolder.resolve("json")
-    private val L = mutableListOf<ReposSearch>()
+    private val queue = mutableListOf<ReposSearch>()
     private val output = File(workingFolder, "github-url-list.txt")
+
     fun createTxt(): File {
         cacheFolder.mkdirs()
         jsonFolder.deleteRecursively()
@@ -38,12 +39,12 @@ class GithubQueryTool(val workingFolder: File, val dateRange: DateRange, val que
 
         output.writeText("")
 
-        L.add(ReposSearch(dateRange, querySpecification))
-        while (L.isNotEmpty()) {
-            val search = L.removeAt(0)
+        queue.add(ReposSearch(dateRange, querySpecification))
+        while (queue.isNotEmpty()) {
+            val search = queue.removeAt(0)
             val result = search.execute()
             if (result.total_count > 1000)
-                L.addAll(0, search.split())
+                queue.addAll(0, search.split())
             else {
                 result.save()
                 search.followingPages(result)
@@ -53,20 +54,24 @@ class GithubQueryTool(val workingFolder: File, val dateRange: DateRange, val que
         return output
     }
 
-    inner class ReposSearch(val dateRange: DateRange, val querySpecification: String, val page: Int = 1) {
-        override fun toString() = "${dateRange.qry} qs=$querySpecification page$page"
+    inner class ReposSearch(val dateRange: DateRange, val querySpec: String, val page: Int = 1) {
 
+        override fun toString() = "${dateRange.qry} qs=$querySpec page$page"
         private val jsonFile = cacheFolder.resolve("${dateRange.fs}-p$page.json")
 
         fun execute(): SearchResult {
             val url = "https://api.github.com/search/repositories?" +
-                    "q=$querySpecification created:${dateRange.qry}&page=$page&per_page=100"
+                    "q=$querySpec created:${dateRange.qry}&page=$page&per_page=100"
                         .replace(" ", "+")
 
-            println(url + (if (jsonFile.exists()) " skipping, file already exists" else ""))
+            print(url)
 
-            if (!jsonFile.exists())
+            if (jsonFile.exists())
+                println(" skipping, file already exists")
+            else {
+                println()
                 invokeApi(url)
+            }
 
 
             val content = jsonFile.readLines().drop(1).joinToString("\n")
@@ -104,13 +109,13 @@ class GithubQueryTool(val workingFolder: File, val dateRange: DateRange, val que
 
         fun split(): List<ReposSearch> {
             val (left, right) = dateRange.split()
-            return listOf(ReposSearch(left, querySpecification), ReposSearch(right, querySpecification))
+            return listOf(ReposSearch(left, querySpec), ReposSearch(right, querySpec))
         }
 
         fun followingPages(r: SearchResult): Sequence<ReposSearch> {
             val pageCount = ceil(r.total_count.toDouble() / 100).toInt()
             if (pageCount <= 1) return emptySequence()
-            return (2..pageCount).map { ReposSearch(dateRange, querySpecification, it) }.asSequence()
+            return (2..pageCount).map { ReposSearch(dateRange, querySpec, it) }.asSequence()
         }
 
         inner class SearchResult(jsonContent: String) {
@@ -148,13 +153,15 @@ class DateRange(val dtStart: DateTime, val dtEnd: DateTime) {
 
     val days: Int = Interval(dtStart, dtEnd.plusDays(1)).toDuration().toStandardDays().days
 
+    private fun DateTime.yyyyMMdd() = toString("yyyy-MM-dd").orEmpty()
+
     val start = dtStart.yyyyMMdd()
     val end = dtEnd.yyyyMMdd()
     val qry = "$start..$end"
-    val fs = "$start-$end"
+    val fs = "$start--$end"
 
     override fun toString() = qry
 }
 
-private fun DateTime.yyyyMMdd() = toString("yyyy-MM-dd").orEmpty()
+
 
