bf58c88dd72989f218d2c7f4657747189cec171a
Simone Giacomelli <simone.giacomelli@gmail.com>
2020-02-28 11:33:21 +0100

    DateRange added good time support

--- src/jvmMain/kotlin/satd/github/DateRange.kt
+++ src/jvmMain/kotlin/satd/github/DateRange.kt
@@ -2,11 +2,12 @@ package satd.github
 
 import org.joda.time.DateTime
 import org.joda.time.Interval
+import org.joda.time.LocalTime
 import kotlin.math.max
 
 class DateRange(val dtStart: DateTime, val dtEnd: DateTime) {
     fun split(): Pair<DateRange, DateRange> {
-        if (sameDay()) throw Exception("Cannot split one day :(")
+        if (sameDay()) return splitSameDay()
 
         val days1 = days / 2
         val days2 = days - days1
@@ -17,15 +18,33 @@ class DateRange(val dtStart: DateTime, val dtEnd: DateTime) {
         return Pair(left, right)
     }
 
+    private fun splitSameDay(): Pair<DateRange, DateRange> {
+        val secs1 = seconds / 2
+        val secs2 = seconds - secs1
+        val secs = max(secs1, secs2) - 1
+
+        val left = DateRange(dtStart, dtStart.plusSeconds(secs))
+        val right = DateRange(dtStart.plusSeconds(secs + 1), dtEndAdjusted)
+        return Pair(left, right)
+    }
+
     val days: Int = Interval(dtStart, dtEnd.plusDays(1)).toDuration().toStandardDays().days
 
     private fun DateTime.yyyyMMdd() = toString("yyyy-MM-dd").orEmpty()
 
-    val start = dtStart.yyyyMMdd()
-    val end = dtEnd.yyyyMMdd()
+    private fun DateTime.format() = if (!sameDay()) yyyyMMdd() else
+        toString("yyyy-MM-dd'T'HH:mm:ss'Z'").orEmpty()
+
+
+    val start = dtStart.format()
+    val end = dtEnd.format()
     val qry = "$start..$end"
     val fs = "$start--$end"
 
+    private val dtEndAdjusted: DateTime = if (dtEnd.toLocalTime() == LocalTime.MIDNIGHT)
+        dtEnd.plusDays(1).minusSeconds(1) else dtEnd
+    val seconds: Int = Interval(dtStart, dtEndAdjusted.plusSeconds(1)).toDuration().toStandardSeconds().seconds
+
     override fun toString() = qry
-    fun sameDay() = dtStart == dtEnd
+    fun sameDay() = dtStart.yyyyMMdd() == dtEnd.yyyyMMdd()
 }
--- src/jvmTest/kotlin/satd/github/DateRangeTest.kt
+++ src/jvmTest/kotlin/satd/github/DateRangeTest.kt
@@ -7,12 +7,12 @@ import kotlin.test.*
 class DateRangeTest {
 
     @Test
-    fun testDays(){
+    fun testDays() {
         val dtStart = DateTime.parse("2020-01-01")
         val dtEnd = DateTime.parse("2020-01-06")
 
         val target = DateRange(dtStart, dtEnd)
-        assertEquals(6,target.days)
+        assertEquals(6, target.days)
     }
 
     @Test
@@ -38,18 +38,91 @@ class DateRangeTest {
     }
 
     @Test
+    fun testSameDay_ExternalBorderDatesAreCorrect() {
+        val dtStart = DateTime.parse("2020-01-01")
+        val dtEnd = DateTime.parse("2020-01-01")
+
+        val target = DateRange(dtStart, dtEnd)
+        val (left, right) = target.split()
+        assertEquals("2020-01-01T00:00:00Z", left.start)
+        assertEquals("2020-01-01T23:59:59Z", right.end)
+    }
+
+    @Test
+    fun testSameDay_InternalBorderDatesAreCorrect() {
+        val dtStart = DateTime.parse("2020-01-01")
+        val dtEnd = DateTime.parse("2020-01-01")
+
+        val target = DateRange(dtStart, dtEnd)
+        val (left, right) = target.split()
+        assertEquals("2020-01-01T11:59:59Z", left.end)
+        assertEquals("2020-01-01T12:00:00Z", right.start)
+    }
+
+    @Test
+    fun testSameDay_withTime_ExternalBorderDatesAreCorrect() {
+        val dtStart = DateTime.parse("2020-01-01T01:02:03")
+        val dtEnd = DateTime.parse("2020-01-01T12:13:14")
+
+        val target = DateRange(dtStart, dtEnd)
+        val (left, right) = target.split()
+        assertEquals("2020-01-01T01:02:03Z", left.start)
+        assertEquals("2020-01-01T12:13:14Z", right.end)
+    }
+
+    @Test
+    fun testSameDay_withTime_InternalBorderDatesAreCorrect() {
+        val dtStart = DateTime.parse("2020-01-01T00:00:03")
+        val dtEnd = DateTime.parse("2020-01-01T00:00:08")
+
+        val target = DateRange(dtStart, dtEnd)
+        val (left, right) = target.split()
+        assertEquals("2020-01-01T00:00:05Z", left.end)
+        assertEquals("2020-01-01T00:00:06Z", right.start)
+    }
+
+    @Test
+    fun testSameDay_withTime_TotalIsCorrect() {
+        val dtStart = DateTime.parse("2020-01-01T00:00:03")
+        val dtEnd = DateTime.parse("2020-01-01T00:00:04")
+
+        val target = DateRange(dtStart, dtEnd)
+        val (left, right) = target.split()
+        assertEquals(target.seconds, left.seconds + right.seconds)
+    }
+
+    @Test
+    fun testSameDay_withTime_TotalIsCorrect_odd() {
+        val dtStart = DateTime.parse("2020-01-01T00:00:03")
+        val dtEnd = DateTime.parse("2020-01-01T00:00:05")
+
+        val target = DateRange(dtStart, dtEnd)
+        val (left, right) = target.split()
+        assertEquals(target.seconds, left.seconds + right.seconds)
+    }
+
+    @Test
     fun testTotalIsCorrect() {
         val dtStart = DateTime.parse("2000-01-01")
         val dtEnd = DateTime.parse("2021-12-31")
 
         val target = DateRange(dtStart, dtEnd)
         val (left, right) = target.split()
-        assertEquals(target.days,left.days+right.days)
+        assertEquals(target.days, left.days + right.days)
     }
 
     @Test
     fun testDameDay() {
         val dtStart = DateTime.parse("2000-01-01")
+        val dtEnd = DateTime.parse("2000-01-01")
+
+        val target = DateRange(dtStart, dtEnd)
+        assertTrue(target.sameDay())
+    }
+
+    @Test
+    fun testDameDayWithTime() {
+        val dtStart = DateTime.parse("2000-01-01T01:01:01")
         val dtEnd = DateTime.parse("2000-01-01")
 
         val target = DateRange(dtStart, dtEnd)
