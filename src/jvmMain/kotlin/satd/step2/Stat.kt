aab3530d0961cb818ed2530458c00f6f5a8d5148
Simone Giacomelli <simone.giacomelli@gmail.com>
2020-04-23 17:12:42 +0200

    age statistics

--- src/jvmMain/kotlin/satd/step2/Stat.kt
+++ src/jvmMain/kotlin/satd/step2/Stat.kt
@@ -6,8 +6,11 @@ import satd.utils.Rate
 import satd.utils.Repo
 import satd.utils.logln
 import java.util.concurrent.atomic.AtomicInteger
+import kotlin.math.roundToInt
+import kotlin.math.roundToLong
 
 class Stat(val repo: Repo, commitCount: Int) {
+    val start = System.currentTimeMillis()
 
     /**
      * every invocation will print stats
@@ -31,8 +34,13 @@ class Stat(val repo: Repo, commitCount: Int) {
         AntiSpin(10000) {
             logln(
                 "${repo.urlstr} commit#:${commitRate.spinCount}/$commitCount commit/sec:$commitRate source#:${sourceRate.spinCount}  satd#:${satdRate.spinCount} " +
-                        "satd/sec: $satdRate source/sec:$sourceRate"
+                        "satd/sec: $satdRate source/sec:$sourceRate ${age()}"
             )
         }
 
+    private fun age(): String {
+        val delta = System.currentTimeMillis() - start
+        val mins = (delta.toDouble() / 100 / 60).roundToLong().toDouble() / 10
+        return "age mins:$mins"
+    }
 }
