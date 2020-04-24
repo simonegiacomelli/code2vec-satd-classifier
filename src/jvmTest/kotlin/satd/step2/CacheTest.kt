1d6a4894971bdcb05fb32c5018b27cf9dcacb4f8
Simone Giacomelli <simone.giacomelli@gmail.com>
2020-04-24 19:04:35 +0200

    new test case

--- src/jvmTest/kotlin/satd/step2/CacheTest.kt
+++ src/jvmTest/kotlin/satd/step2/CacheTest.kt
@@ -1,6 +1,8 @@
 package satd.step2
 
 
+import java.io.File
+import kotlin.random.Random
 import kotlin.test.*
 
 class CacheTest {
@@ -17,5 +19,15 @@ class CacheTest {
         val t2 = Cache(file)
         t2.load()
         assertEquals("world", target["hello"])
+    }
+
+    @Test
+    fun verifyNoFileCreated() {
+        val file = File("non-existant-file" + Random.nextLong() + ".txt")
+        val target = Cache(file)
+
+        target["hello"] = "world"
+
+        assertFalse { target.exists() }
     }
 }
