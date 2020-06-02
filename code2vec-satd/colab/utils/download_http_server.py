50746bf60834c9a69f9938fcf2fac35201d9cf46
Simone Giacomelli <simone.giacomelli@gmail.com>
2020-06-02 19:58:50 +0200

    download file from python -m http.server

---
+++ code2vec-satd/colab/utils/download_http_server.py
@@ -0,0 +1,39 @@
+import urllib.request
+from html.parser import HTMLParser
+import os
+
+
+class CatchStartTag(HTMLParser):
+    def __init__(self, href_callback):
+        super().__init__()
+        self.href_callback = href_callback
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'a':
+            self.href_callback(dict(attrs)['href'])
+
+
+def download_files_in_url(start_url, start_folder):
+    todo = []
+
+    def add(u, f):
+        print(u, f)
+        todo.append((u, f))
+
+    add(start_url, start_folder)
+    while len(todo) > 0:
+        url, folder = todo.pop()
+        if url.endswith('/'):
+            with urllib.request.urlopen(url) as fp:
+                html = fp.read().decode('utf8')
+            CatchStartTag(lambda h: add(url + h,
+                                        os.path.join(folder, h) if h.endswith('/') else folder)).feed(html)
+        else:
+            os.makedirs(folder, exist_ok=True)
+            filename = os.path.join(folder, url.split('/')[-1])
+            if not os.path.exists(filename):
+                urllib.request.urlretrieve(url, filename)
+
+
+if __name__ == '__main__':
+    download_files_in_url('http://foo.inf.usi.ch:8000/', './temp/download')
