3ee051fe868272430fb24023267dc74fc43dd32b
Simone Giacomelli <simone.giacomelli@gmail.com>
2020-06-05 19:54:19 +0200

    development hyperopt parallel pipeline

--- code2vec-satd/hyperopt_mongo_worker.py
+++ code2vec-satd/hyperopt_mongo_worker.py
@@ -8,7 +8,7 @@ from hyperopt_mongo_prop import MongoProp
 
 if __name__ == '__main__':
     print('sys.argv', sys.argv)
-    sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
+    sys.argv[0] = os.path.abspath(re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0]))
     print('new  sys.argv[0]', sys.argv[0])
     sys.argv.append('--mongo=' + MongoProp().mongo_url_worker())
     sys.argv.append('--workdir=' + os.getcwd())
