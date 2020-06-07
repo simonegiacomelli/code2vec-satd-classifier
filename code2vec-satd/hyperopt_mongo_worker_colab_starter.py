47612f40fc5764c6d32825aece77409d1536550b
Simone Giacomelli <simone.giacomelli@gmail.com>
2020-06-07 11:00:35 +0200

    initial development colab starter

---
+++ code2vec-satd/hyperopt_mongo_worker_colab_starter.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import re
+import sys
+import os
+from hyperopt.mongoexp import main
+from hyperopt_mongo_prop import MongoProp
+
+if __name__ == '__main__':
+    import os
+    #!pip install hyperopt==0.2.4 --upgrade
+    print('ok, start!')
+    os.system('whoami')
