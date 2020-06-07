0936e2c8c7d93eda6fa1b9872c59e97fb3a31f58
Simone Giacomelli <simone.giacomelli@gmail.com>
2020-06-07 12:56:49 +0200

    faster tests

--- code2vec-satd/hyperopt_mongo_worker_colab_starter.py
+++ code2vec-satd/hyperopt_mongo_worker_colab_starter.py
@@ -43,7 +43,9 @@ if __name__ == '__main__':
         # system_log("runuser -l postgres -c 'cd ./code2vec-satd-classifier &&  python3 code2vec-satd/colab/utils/download_http_server.py --url  http://foo.inf.usi.ch:8000/ --folder ./satd-classifier/data/backup/bk1'")
         # sadly, for how the pgsql restore program works, it is expected to receive an error exit code
         # system_log("runuser -l postgres -c 'cd ./code2vec-satd-classifier/satd-classifier && ./gradlew pgsqlRestore --console=plain'", raise_exception=False)
-        system_log("runuser -l postgres -c 'cd ./code2vec-satd-classifier/satd-classifier && ./gradlew showdatabase </dev/null > ~/pgsql-out.txt 2>&1 & disown '")
+        import subprocess
+
+        subprocess.call("runuser -l postgres -c 'cd ./code2vec-satd-classifier/satd-classifier && ./gradlew showdatabase </dev/null > ~/pgsql-out.txt 2>&1 & disown '")
 
     # system_log('pip install hyperopt==0.2.4')
     # system_log('pip install tensorflow==2.1.0')
