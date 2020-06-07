f5efa6d683b3b691885819461157c269e55c9451
Simone Giacomelli <simone.giacomelli@gmail.com>
2020-06-07 19:37:50 +0200

    switching to optuna

--- code2vec-satd/satd_colab_starter.py
+++ code2vec-satd/satd_colab_starter.py
@@ -11,6 +11,9 @@ def system_log(cmd, raise_exception=True):
 
 def main():
     print('ok, start!')
+    system_log(
+        'cd /content/code2vec-satd-classifier/satd-classifier && cd data/pgsql || unzip -q ./pgsql_binaries/pgsql_linux.zip -d ./data && echo unzip done')
+
     # check user postgres
     from optuna_properties import prefetch_file_properties
     prefetch_file_properties()
@@ -25,26 +28,20 @@ def main():
 
         for cmd in create_postgres_user.split('\n'):
             code = system_log(cmd.strip())
-
-    print('pgsql out exists:', os.path.exists('/home/postgres/pgsql-out.txt'))
-    if not os.path.exists('/home/postgres/pgsql-out.txt'):
-        print('checking sources for user postgres and db files')
+    print('pgsql out exists:', os.path.exists('/content/pgsql-out.txt'))
+    if not os.path.exists('/content/pgsql-out.txt'):
         system_log(
-            'runuser -l postgres -c "cd; cd code2vec-satd-classifier && git pull || git clone https://github.com/simonegiacomelli/code2vec-satd-classifier"')
-        system_log(
-            "runuser -l postgres -c 'cd; cd ./code2vec-satd-classifier/satd-classifier && cd data/pgsql || unzip -q ./pgsql_binaries/pgsql_linux.zip -d ./data && echo unzip done' ")
-        system_log(
-            "runuser -l postgres -c 'cd ./code2vec-satd-classifier &&  python3 code2vec-satd/colab/utils/download_http_server.py --url  http://foo.inf.usi.ch:8000/ --folder ./satd-classifier/data/backup/bk1'")
+            "cd /content/code2vec-satd-classifier &&  python3 code2vec-satd/colab/utils/download_http_server.py --url  http://foo.inf.usi.ch:8000/ --folder ./satd-classifier/data/backup/bk1'")
         # sadly, for how the pgsql restore program works, it is expected to receive an error exit code
+        system_log('chown -R postgres:postgres /content')
         system_log(
-            "runuser -l postgres -c 'cd ./code2vec-satd-classifier/satd-classifier && ./gradlew pgsqlRestore --console=plain > ~/pgsql-restore-out.txt 2>&1'",
+            "runuser -l postgres -c 'cd /content/code2vec-satd-classifier/satd-classifier && ./gradlew pgsqlRestore --console=plain -Dorg.gradle.daemon=false > /content/pgsql-restore-out.txt 2>&1'",
             raise_exception=False)
-
         get_ipython().system_raw(
-            "runuser -l postgres -c 'cd ./code2vec-satd-classifier/satd-classifier && ./gradlew showdatabase  </dev/null > ~/pgsql-out.txt 2>&1 & disown '")
+            "runuser -l postgres -c 'cd /content/code2vec-satd-classifier/satd-classifier && ./gradlew pgsqlStartInstance --console=plain -Dorg.gradle.daemon=false  </dev/null > /content/pgsql-out.txt 2>&1 & disown '")
 
-    system_log('pip install optuna')
-    system_log('pip install tensorflow==2.1.0')
+    #system_log('pip install optuna')
+    #system_log('pip install tensorflow==2.1.0')
 
     print('done')
 
