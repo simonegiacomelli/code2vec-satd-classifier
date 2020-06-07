9e045e2613cea834994028f6ad534aa80f44534c
Simone Giacomelli <simone.giacomelli@gmail.com>
2020-06-07 18:29:09 +0200

    switching to optuna

--- code2vec-satd/satd_colab_starter.py
+++ code2vec-satd/satd_colab_starter.py
@@ -12,6 +12,9 @@ def system_log(cmd, raise_exception=True):
 def main():
     print('ok, start!')
     # check user postgres
+    from optuna_properties import get_file_properties
+    get_file_properties()
+
     if system_log('runuser -l postgres -c "cd"', raise_exception=False) == 0:
         print('user postgres do exists.')
     else:
