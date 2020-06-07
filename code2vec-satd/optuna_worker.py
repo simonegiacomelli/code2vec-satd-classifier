aa8d98b6ab4262677487b40b62cf66fa23b1325a
Simone Giacomelli <simone.giacomelli@gmail.com>
2020-06-07 17:14:10 +0200

    switching to optuna

--- code2vec-satd/optuna_worker.py
+++ code2vec-satd/optuna_worker.py
@@ -7,24 +7,21 @@ from satd_utils import prop2dict
 
 
 def objective(trial):
-    # x = trial.suggest_uniform('x', -10, 10)
-    # return (x - 2) ** 2
 
-    clean_token_count_limit = int(trial.suggest_discrete_uniform('clean_token_count_limit', 20, 60000, 1))
-    return float(clean_token_count_limit) ** 2
+    clean_token_count_limit = int(trial.suggest_discrete_uniform('clean_token_count_limit', 20, 60, 1))
 
     clean_token_count_limit = int(clean_token_count_limit)
     evaluation, info, output = full_pipeline.run(clean_token_count_limit)
     accuracy_str = prop2dict(evaluation)['accuracy']
     accuracy = float(accuracy_str)
-    return {
+    user_data = {
         'loss': 1.0 - accuracy,
         # -- store other results like this
         'os_uname': os.uname(),
         'evaluation': evaluation,
         'attachments': {'info': info, 'output': output}
     }
-
+    trial.set_user_attr('user_data', user_data)
 
 if __name__ == '__main__':
     from file_properties import FileProperties
