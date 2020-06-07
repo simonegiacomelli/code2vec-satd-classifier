444922e0fcd4111926e872ba585d9dcc2d381ce3
Simone Giacomelli <simone.giacomelli@gmail.com>
2020-06-07 14:52:15 +0200

    faster tests

--- code2vec-satd/hyperopt_mongo_worker_colab_starter.py
+++ code2vec-satd/hyperopt_mongo_worker_colab_starter.py
@@ -30,16 +30,15 @@ def main():
             'runuser -l postgres -c "cd; cd code2vec-satd-classifier && git pull || git clone https://github.com/simonegiacomelli/code2vec-satd-classifier"')
         system_log(
             "runuser -l postgres -c 'cd; cd ./code2vec-satd-classifier/satd-classifier && cd data/pgsql || unzip -q ./pgsql_binaries/pgsql_linux.zip -d ./data && echo unzip done' ")
-        ###        system_log("runuser -l postgres -c 'cd ./code2vec-satd-classifier &&  python3 code2vec-satd/colab/utils/download_http_server.py --url  http://foo.inf.usi.ch:8000/ --folder ./satd-classifier/data/backup/bk1'")
+        system_log("runuser -l postgres -c 'cd ./code2vec-satd-classifier &&  python3 code2vec-satd/colab/utils/download_http_server.py --url  http://foo.inf.usi.ch:8000/ --folder ./satd-classifier/data/backup/bk1'")
         # sadly, for how the pgsql restore program works, it is expected to receive an error exit code
-        ###        system_log("runuser -l postgres -c 'cd ./code2vec-satd-classifier/satd-classifier && ./gradlew pgsqlRestore --console=plain'", raise_exception=False)
-        # import subprocess
+        system_log("runuser -l postgres -c 'cd ./code2vec-satd-classifier/satd-classifier && ./gradlew pgsqlRestore --console=plain'", raise_exception=False)
 
         get_ipython().system_raw(
-            "runuser -l postgres -c 'cd ./code2vec-satd-classifier/satd-classifier && ./gradlew showdatabase  </dev/null > ~/pgsql-out2.txt 2>&1 & disown '")
+            "runuser -l postgres -c 'cd ./code2vec-satd-classifier/satd-classifier && ./gradlew showdatabase  </dev/null > ~/pgsql-out.txt 2>&1 & disown '")
 
-    # system_log('pip install hyperopt==0.2.4')
-    # system_log('pip install tensorflow==2.1.0')
+    system_log('pip install hyperopt==0.2.4')
+    system_log('pip install tensorflow==2.1.0')
 
     print('done')
 
