f72c246142ca4c2658fcb2e93df48e3f7aec93c9
Simone Giacomelli <simone.giacomelli@gmail.com>
2020-06-07 10:17:15 +0200

    fixed task

--- satd-classifier/src/jvmMain/kotlin/satd/step2/MainStartSqlConsole.kt
+++ satd-classifier/src/jvmMain/kotlin/satd/step2/MainStartSqlConsole.kt
@@ -1,10 +1,11 @@
 package satd.step2
 
 import org.h2.tools.Server
+import satd.utils.hostname
 import satd.utils.logln
 
 fun main() {
     persistence.setupDatabase()
-    logln("Connect to http://servername:61603")
+    logln("Connect to http://${hostname}:61603")
     Server.createWebServer("-webPort", "61603","-webAllowOthers").start()
 }
