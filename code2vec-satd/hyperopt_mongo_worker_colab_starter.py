6c170c4e05dcc83995d8b4f248b502ecf51b87bc
Simone Giacomelli <simone.giacomelli@gmail.com>
2020-06-07 12:57:59 +0200

    faster tests

--- code2vec-satd/hyperopt_mongo_worker_colab_starter.py
+++ code2vec-satd/hyperopt_mongo_worker_colab_starter.py
@@ -45,7 +45,7 @@ if __name__ == '__main__':
         # system_log("runuser -l postgres -c 'cd ./code2vec-satd-classifier/satd-classifier && ./gradlew pgsqlRestore --console=plain'", raise_exception=False)
         import subprocess
 
-        subprocess.call("runuser -l postgres -c 'cd ./code2vec-satd-classifier/satd-classifier && ./gradlew showdatabase </dev/null > ~/pgsql-out.txt 2>&1 & disown '")
+        subprocess.call("/sbin/runuser -l postgres -c 'cd ./code2vec-satd-classifier/satd-classifier && ./gradlew showdatabase </dev/null > ~/pgsql-out.txt 2>&1 & disown '")
 
     # system_log('pip install hyperopt==0.2.4')
     # system_log('pip install tensorflow==2.1.0')
