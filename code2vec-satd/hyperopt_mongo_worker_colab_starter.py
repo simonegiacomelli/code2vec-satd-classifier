fed9648227a73f62cbec4285a5349749a3594317
Simone Giacomelli <simone.giacomelli@gmail.com>
2020-06-07 12:01:34 +0200

    development colab starter

--- code2vec-satd/hyperopt_mongo_worker_colab_starter.py
+++ code2vec-satd/hyperopt_mongo_worker_colab_starter.py
@@ -6,8 +6,46 @@ import os
 from hyperopt.mongoexp import main
 from hyperopt_mongo_prop import MongoProp
 
+create_postgres_user = """useradd -s /bin/bash -p $(openssl passwd -1 postgres) postgres
+mkdir /home/postgres 
+chown postgres:postgres -R /home/postgres"""
+
+
+# import subprocess
+#    import sys
+#    subprocess.check_call([sys.executable, "-m", "pip"] + args)
+
+def system_log(cmd, raise_exception=True):
+    print('Executing', cmd)
+    code = os.system(cmd)
+    if raise_exception and code != 0:
+        raise Exception('Bad exit code', code)
+    return code
+
+
 if __name__ == '__main__':
     import os
-    #!pip install hyperopt==0.2.4 --upgrade
+
     print('ok, start!')
-    os.system('whoami')
+    # check user postgres
+    if system_log('runuser -l postgres -c "cd"', raise_exception=False) == 0:
+        print('user postgres do exists.')
+    else:
+        print('user postgres do not exists. Going to create it')
+        for cmd in create_postgres_user.split('\n'):
+            code = system_log(cmd)
+
+
+    print('pgsql out exists:', os.path.exists('/home/postgres/pgsql-out.txt'))
+    if not os.path.exists('/home/postgres/pgsql-out.txt'):
+        print('checking sources for user postgres and db files')
+        system_log('runuser -l postgres -c "cd; cd code2vec-satd-classifier && git pull || git clone https://github.com/simonegiacomelli/code2vec-satd-classifier"')
+        system_log("runuser -l postgres -c 'cd; cd ./code2vec-satd-classifier/satd-classifier && cd data/pgsql || unzip -q ./pgsql_binaries/pgsql_linux.zip -d ./data && echo unzip done' ")
+        system_log("runuser -l postgres -c 'cd ./code2vec-satd-classifier &&  python3 code2vec-satd/colab/utils/download_http_server.py --url  http://foo.inf.usi.ch:8000/ --folder ./satd-classifier/data/backup/bk1'")
+        # sadly, for how the pgsql restore program works, it is expected to receive an error exit code
+        system_log("runuser -l postgres -c 'cd ./code2vec-satd-classifier/satd-classifier && ./gradlew pgsqlRestore --console=plain'", raise_exception=False)
+        system_log("runuser -l postgres -c 'cd ./code2vec-satd-classifier/satd-classifier && ./gradlew showdatabase > ~/pgsql-out.txt 2>&1 & disown'")
+    # system_log('pip install hyperopt==0.2.4')
+    # system_log('pip install tensorflow==2.1.0')
+
+    print('done')
