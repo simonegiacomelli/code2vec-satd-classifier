31799def936732ee4074b1192cd012d4f3a01096
Simone Giacomelli <simone.giacomelli@gmail.com>
2020-06-08 18:24:32 +0200

    add optuna visualization

--- code2vec-satd/optuna_visualization.py
+++ code2vec-satd/optuna_visualization.py
@@ -5,20 +5,15 @@ import optuna
 import full_pipeline
 from satd_utils import prop2dict
 
-
 if __name__ == '__main__':
     from optuna_properties import get_file_properties
     from optuna.visualization import *
+
     prop = get_file_properties()
     db_url = prop.db_url
     study_name = prop.study_name
     # db_url=sqlite:///example.db\nstudy_name=exp1
     # study_name = 'example-study'  # Unique identifier of the study.
     study = optuna.create_study(study_name=study_name, storage=db_url, load_if_exists=True)
-    plot_contour(study, params=['learning_rate',
-                                'max_depth',
-                                'num_leaves',
-                                'min_data_in_leaf',
-                                'feature_fraction',
-                                'subsample'])
+    plot_optimization_history(study)
     print('done')
